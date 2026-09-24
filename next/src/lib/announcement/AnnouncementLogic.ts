81737cd7c13b6f776ea4d4070462258c9a8ed128
windsnow1025 <windsnow1025@gmail.com>
2026-09-24 10:31:22 -0400

    chore(next): minor rename

--- next/src/lib/announcement/AnnouncementLogic.ts
+++ next/src/lib/announcement/AnnouncementLogic.ts
@@ -3,15 +3,15 @@ import {handleError} from "@/lib/common/ErrorHandler";
 import {AnnouncementResDto} from "@/client/nest";
 
 export default class AnnouncementLogic {
-  private announcementService: AnnouncementClient;
+  private announcementClient: AnnouncementClient;
 
   constructor() {
-    this.announcementService = new AnnouncementClient();
+    this.announcementClient = new AnnouncementClient();
   }
 
   async fetchAnnouncement(): Promise<AnnouncementResDto> {
     try {
-      return await this.announcementService.fetchAnnouncement();
+      return await this.announcementClient.fetchAnnouncement();
     } catch (error) {
       handleError(error, "Failed to fetch announcement");
     }
@@ -19,7 +19,7 @@ export default class AnnouncementLogic {
 
   async updateAnnouncement(content: string): Promise<AnnouncementResDto> {
     try {
-      return await this.announcementService.updateAnnouncement(content);
+      return await this.announcementClient.updateAnnouncement(content);
     } catch (error) {
       handleError(error, "Failed to update announcement");
     }
